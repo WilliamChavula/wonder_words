d1289a03f1de847715d1c382551135650248ea7a
williamchavula <rumbani@gmail.com>
2024-07-17 10:08:10 -0500

    chore: initialize QuoteList project

---
+++ QuoteList/Extensions/ConfigureQuotesDetail.cs
@@ -0,0 +1,9 @@
+namespace QuoteList.Extensions;
+
+public static class ConfigureQuotesDetail
+{
+    public static MauiAppBuilder UseQuotesDetail(this MauiAppBuilder builder)
+    {
+        return builder;
+    }
+}
